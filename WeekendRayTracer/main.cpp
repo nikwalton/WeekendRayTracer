0782a6818d0c52985cb1c9355eca4acf3e0250c7
Nikolaus Walton <nikawalton2@gmail.com>
2021-07-11 07:51:27 -0700

    Finished Chapter 2, Reached Hellow World image of graphics

--- WeekendRayTracer/main.cpp
+++ WeekendRayTracer/main.cpp
@@ -7,8 +7,31 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
-	return 0;
+int main() {
+
+	// Image
+	const int width = 256;
+	const int height = 256;
+	
+	// Render
+	std::cout << "P3\n" << width << ' ' << height << "\n255\n";
+	
+	//pixels are written in rows from left to right
+	//rows are written top to bottom
+	
+	for (int j = height - 1; j>= 0; --j) {
+		for (int i = 0; i < width; ++ i) {
+			
+			//rgb ranges from 0.0 to 1.0
+			auto r = double(i) / (width - 1);
+			auto g = double(j) / (height - 1);
+			auto b = 0.25;
+			
+			int ir = static_cast<int>(255.999 * r);
+			int ig = static_cast<int>(255.999 * g);
+			int ib = static_cast<int>(255.999 * b);
+			
+			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+		}
+	}
 }
