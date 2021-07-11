fb03636a4197e8785d2f1985a94a06b95ea2c601
Nikolaus Walton <nikawalton2@gmail.com>
2021-07-11 07:35:13 -0700

    Initial Commit

---
+++ WeekendRayTracer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WeekendRayTracer
+//
+//  Created by Nikolaus Walton on 7/11/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
